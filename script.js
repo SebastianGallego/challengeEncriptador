4e6c613f184e78f4bbde3320ad96af43a5741094
Sebastian Gallego <sebastian.gallego@gmail.com>
2023-06-02 22:17:02 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+const textoEntrada = document.querySelector(".text-entrada"); // textArea de Entrada
+const textoSalida = document.querySelector(".text-salida");   //textArea de salida
+
+const matrizLlave = [ 
+    ["a", "ai"],
+    ["e", "enter"], 
+    ["i", "imes"],
+    ["o", "ober"],
+    ["u", "ufat"]
+]
+
+function btnEncriptar(){
+    const textoEncriptado = encriptar(text-entrada.value);
+
+    text-salida.value = textoEncriptado;
+    text-entrada.value = "";
+
+}
+
+function encriptar(cadena){
+    cadena=cadena.toLowerCase();
+
+    for(let i=0; i<matrizLlave.length;i++){
+
+
+
+        
+    }
+
+}
