d99d3270a4447a438fa598668b11b62e08ad0e4a
Sebastian Gallego <sebastian.gallego@gmail.com>
2023-06-05 13:55:47 -0300

    version1.0

--- script.js
+++ script.js
@@ -6,24 +6,51 @@
 //     "u", "ufat"
 // 
 
-let textoEntrada = document.getElementById("entrada").value; // textArea de Entrada
+let textoEntrada = document.getElementById("entrada"); // textArea de Entrada
 let textoSalida = document.getElementById("salida"); // textArea de Entrada
 
 
-console.log(textoEntrada);
-console.log(textoEntrada.length);
-
 function btnEncriptar(){
     
-    textoEntrada = document.getElementById("entrada").value
-
-    if(textoEntrada.length != 0) {
-        let textoEncriptado = textoEntrada
+    let texto = document.getElementById("entrada").value
+    
+    if(texto.length != 0) {
+        let textoEncriptado = texto
             .replace(/e/gi, "enter")
             .replace(/i/gi, "imes")
             .replace(/a/gi, "ai")
             .replace(/o/gi, "ober")
-            .replace(/u/gi, "ufat");
+            .replace(/u/gi, "ufat")
+            .replace(/é/gi, "enter")
+            .replace(/í/gi, "imes")
+            .replace(/á/gi, "ai")
+            .replace(/ó/gi, "ober")
+            .replace(/ú/gi, "ufat");
+
+        
+        textoSalida.textContent = textoEncriptado;
+        textoSalida.style.backgroundImage = "url('encriptado.png')";
+        textoSalida.style.backgroundSize="30%";
+
+    }
+    else {
+        alert("El textarea está vacío. Por favor, ingresa un mensaje.");
+        return; // 
+    }
+} 
+
+
+function btnDesencriptar() {
+
+    let texto = document.getElementById("entrada").value
+    
+    if(texto.length != 0) {
+        let textoEncriptado = texto
+            .replace(/enter/gi, "e")
+            .replace(/imes/gi, "i")
+            .replace(/ai/gi, "a")
+            .replace(/ober/gi, "o")
+            .replace(/ufat/gi, "u");
 
         
         textoSalida.textContent = textoEncriptado;
@@ -35,7 +62,7 @@ function btnEncriptar(){
         alert("El textarea está vacío. Por favor, ingresa un mensaje.");
         return; // Detiene la ejecución de la función si el textarea está vacío
     }
-} 
+}
 
 
 function copiarAlPortapapeles() {
@@ -44,7 +71,6 @@ function copiarAlPortapapeles() {
     document.execCommand("copy");
     
     textoEntrada.value = "";
-    
     location.reload(); // recargo la pagina para limpiar reiniciar todo.    
 
   }
